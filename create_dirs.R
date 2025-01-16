38c3f6695858152d69e79bde25917f500ea5b5eb
Bicko Cygu <cygu@aims.ac.za>
2025-01-16 11:50:22 +0300

    Delete create_dirs.R

--- create_dirs.R
+++
@@ -1,7 +0,0 @@
-##### ---- Folder setup  ------------------------------------------------####
-## Folders
-### Folder to save datasets
-create_dir("datasets")
-### Create logs
-create_dir(".log_files")
-
