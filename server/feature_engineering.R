990aafd58c82940017725471df6ef976805952ba
cygubicko <cygu@aims.ac.za>
2025-04-22 16:22:48 +0300

    Feature engineering

--- server/feature_engineering.R
+++ server/feature_engineering.R
@@ -5,8 +5,12 @@ setup_recipe_server = function() {
 	observeEvent(input$manage_data_apply, {
 		if (isTRUE(!is.null(rv_current$working_df))) {
 			if (isTRUE(!is.null(rv_ml_ai$outcome))) {
-				model
+				rv_ml_ai$model_formula = as.formula(paste0(rv_ml_ai$outcome, "~."))
+			} else {
+				rv_ml_ai$model_formula = NULL
 			}
+		} else {
+			rv_ml_ai$model_formula = NULL
 		}
 	})
 	
