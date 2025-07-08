e85730eca0a04548d24fa98ac1d20b9edf064d2c
cygubicko <cygu@aims.ac.za>
2025-07-09 00:52:41 +0300

    Fix metrics plot

--- server/model_training_caret_models.R
+++ server/model_training_caret_models.R
@@ -822,52 +822,72 @@ model_training_caret_train_metrics_server = function() {
 						 DT::formatRound(columns = c("lower", "estimate", "upper"), digits = 4)
 					})
 					
-					## Test data
-					test_plots = plot(rv_training_results$test_metrics_objs)
-					output$model_training_caret_test_metrics_plot_specifics = renderPlot({
-						plot(test_plots$specifics)	
-					})
-					
-					output$model_training_caret_test_metrics_plot_all = renderPlot({
-						plot(test_plots$all)	
-					})
+					if (isTRUE(!is.null(rv_training_results$test_metrics_objs))) {
+						## Test data
+						test_plots = plot(rv_training_results$test_metrics_objs)
+						output$model_training_caret_test_metrics_plot_specifics = renderPlot({
+							test_plots$specifics
+						})
+						
+						output$model_training_caret_test_metrics_plot_all = renderPlot({
+							test_plots$all	
+						})
 
-					output$model_training_caret_test_metrics_plot_roc = renderPlot({
-						plot(test_plots$roc)	
-					})
-					
-					output$model_training_caret_test_metrics_df = DT::renderDT({
-					  df = rv_training_results$test_metrics_objs$all
+						output$model_training_caret_test_metrics_plot_roc = renderPlot({
+							test_plots$roc
+						})
+						
+						output$model_training_caret_test_metrics_df = DT::renderDT({
+						  df = rv_training_results$test_metrics_objs$all
 
-					  DT::datatable(
-						 df,
-						 escape = FALSE,
-						 rownames = FALSE,
-						 options = list(
-							processing = FALSE,
-							autoWidth = FALSE,
-							scrollX = TRUE,
-							columnDefs = list(
-							  list(className = 'dt-center', targets = c("lower", "estimate", "upper"))
-							)
-						 )
-					  ) %>%
-						 DT::formatRound(columns = c("lower", "estimate", "upper"), digits = 4)
-					})
-					
-
+						  DT::datatable(
+							 df,
+							 escape = FALSE,
+							 rownames = FALSE,
+							 options = list(
+								processing = FALSE,
+								autoWidth = FALSE,
+								scrollX = TRUE,
+								columnDefs = list(
+								  list(className = 'dt-center', targets = c("lower", "estimate", "upper"))
+								)
+							 )
+						  ) %>%
+							 DT::formatRound(columns = c("lower", "estimate", "upper"), digits = 4)
+						})
+					} else {
+						output$model_training_caret_test_metrics_plot_specifics = NULL
+						output$model_training_caret_test_metrics_plot_all = NULL
+						output$model_training_caret_test_metrics_plot_roc = NULL
+						output$model_training_caret_test_metrics_df = NULL
+					}
 
 				} else {
 					output$model_training_caret_train_metrics_plot = NULL
 					output$model_training_caret_train_metrics_df = NULL
+						
+					output$model_training_caret_test_metrics_plot_specifics = NULL
+					output$model_training_caret_test_metrics_plot_all = NULL
+					output$model_training_caret_test_metrics_plot_roc = NULL
+					output$model_training_caret_test_metrics_df = NULL
 				}
 			} else {
 				output$model_training_caret_train_metrics_plot = NULL
 				output$model_training_caret_train_metrics_df = NULL
+					
+				output$model_training_caret_test_metrics_plot_specifics = NULL
+				output$model_training_caret_test_metrics_plot_all = NULL
+				output$model_training_caret_test_metrics_plot_roc = NULL
+				output$model_training_caret_test_metrics_df = NULL
 			}
 		} else {
 			output$model_training_caret_train_metrics_plot = NULL
 			output$model_training_caret_train_metrics_df = NULL
+				
+			output$model_training_caret_test_metrics_plot_specifics = NULL
+			output$model_training_caret_test_metrics_plot_all = NULL
+			output$model_training_caret_test_metrics_plot_roc = NULL
+			output$model_training_caret_test_metrics_df = NULL
 		}
 	})
 	
