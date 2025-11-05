2b207d3d044411c9bd163e410d2272ead2dc4a90
Ousmane Diop <ouze9681@gmail.com>
2025-11-05 20:02:51 +0000

    Testing the merge Pycaret

--- ui/train_all_model_ui.R
+++ ui/train_all_model_ui.R
@@ -1,18 +1,24 @@
 train_all_model_ui = function() {
 	tabItem(tabName = "trainModel",
 		fluidRow(
-			column(width = 12
-				, uiOutput("model_training_setup_presetup")
+			conditionalPanel(
+				condition = "input.modelling_framework_choices == 'Caret'",
+				column(width = 12
+					, uiOutput("model_training_setup_presetup")
 
-			)
-			, column(width=12
-				, uiOutput("model_training_caret_models_ui")
-			)
-			, column(width=12
-				, uiOutput("model_training_caret_train_metrics")
-			)
-			, column(width=12
-				, train_model_ui("train_model")
+				)
+				, column(width=12
+					, uiOutput("model_training_caret_models_ui")
+				)
+				, column(width=12
+					, uiOutput("model_training_caret_train_metrics")
+				)
+			),
+			conditionalPanel(
+				condition = "input.modelling_framework_choices == 'Pycaret'",		
+				column(width=12
+					, train_model_ui("train_model")
+				)
 			)
 		)
 	)
--- ui/validate_deploy_model_ui.R
+++ ui/validate_deploy_model_ui.R
@@ -1,12 +1,19 @@
 validate_deploy_model_ui = function() {
 	tabItem(tabName = "validateDeployModel",
 	  fluidRow(
-		column(width=12
-			, uiOutput("deploy_trained_caret_models_box_ui")
+		conditionalPanel(
+			condition = "input.modelling_framework_choices == 'Caret'",
+			column(width=12
+				, uiOutput("deploy_trained_caret_models_box_ui")
+			)
 		)
-		, column(width=12,
-			deployment_ui("deploy")
+		,conditionalPanel(
+			condition = "input.modelling_framework_choices == 'Pycaret'",
+			column(width=12,
+				deployment_ui("deploy")
+			)
 		)
 	  )
 	)
 }
+
